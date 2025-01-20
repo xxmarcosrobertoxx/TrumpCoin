e4f6c360b603c51d375135a0002e926a88df675c
Marcos Roberto <xxmarcosrobertoxx@gmail.com>
2025-01-20 14:26:13 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-document.getElementById('form-investimento').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    let quantidade = document.getElementById('quantidade').value;
-    let moeda = document.getElementById('moeda').value;
-    
-    alert(`Investimento de ${quantidade} ${moeda} realizado com sucesso!`);
+document.addEventListener("DOMContentLoaded", function() {
+    // Envio de formulário de contato
+    document.getElementById('form-contato').addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        const nome = document.getElementById('nome').value;
+        const email = document.getElementById('email').value;
+        const mensagem = document.getElementById('mensagem').value;
+        
+        alert(`Mensagem enviada com sucesso!\nNome: ${nome}\nE-mail: ${email}\nMensagem: ${mensagem}`);
+    });
+
+    // Investimento
+    document.getElementById('form-investimento').addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const quantidade = document.getElementById('quantidade').value;
+        const moeda = document.getElementById('moeda').value;
+
+        alert(`Investimento de ${quantidade} ${moeda} realizado com sucesso!`);
+    });
 });
